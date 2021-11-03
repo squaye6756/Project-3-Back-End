fc9984510093e98ede633a9f2536a9afac6fd486
Eric Reyes <ericreyes@gmail.com>
2021-11-03 13:00:49 -0500

    routers set up - need schemas for full test

---
+++ controllers/teams_controller.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const router = express.Router()
+
+//INDEX route
+router.get('/', (req, res) => {
+    Teams.find({}, (err, foundTeams) =>{
+        res.json(foundTeams)
+    })
+})
+
+//CREATE route
+router.post('/', (req, res) => {
+    Teams.create(req.body, (err, createdTeams) => {
+        res.json(createdTeam);
+    })
+})
+
+//DELETE route
+router.delete('/:id', (req, res) => {
+    Teams.findByIdAndRemove(req.params.id, (err, deletedTeam) => {
+        res.json(deletedTeam)
+    })
+})
+
+//EDIT route
+
+router.put('/:id', (req, res) => {
+    Teams.findByIdAndUpdate(req.params.id, req.body, { new: true },
+        (err, updatedTeam) => {
+            res.json(updatedTeam)
+        })
+})
+
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const app = express()
-
 const PORT = 3000
 const mongoURI = "mongodb://localhost:27017/streetball"
 
@@ -10,17 +9,18 @@ app.use(express.json())
 app.use(cors())
 
 
+const teamsController = require('./controllers/teams_controller.js')
+app.use('/teams', teamsController)
 
-
-mongoose.connect(mongoURI, () => {
-    console.log('Mongod is reached')
+app.get('/', (req, res) => {
+    res.send('API for streetball teams- requests made at /teams')
 })
+
 
 app.listen(PORT, () => {
     console.log('streetball - listining on port', PORT)
 })
 
-
-
-
-
+mongoose.connect(mongoURI, () => {
+    console.log('Mongod is reached')
+})
